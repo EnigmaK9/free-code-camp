565846b192828348c609d8b70116ab3bbb133169
EnigmaK9 <enigmak9@protonmail.com>
2022-01-27 05:44:00 -0600

    main py changes

--- kccpgm/2-the-galaxy/v1/main.py
+++ kccpgm/2-the-galaxy/v1/main.py
@@ -1,13 +1,30 @@
 from kivy.app import App 
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, Clock
+
 
 
 
 
 
 class MainWidget(Widget):
-    pass
+    perspective_point_x = NumericProperty(0)
+    perspective_point_y = NumericProperty(0)
+    #method to initialize attributes of the created objetc
+    # First method to execute when creating an object
+    # OOP
+    # def __init__([parameters]): 
+    # [algorithm]
+    def __init__(self, **kwargs):
+    #
+        super(MainWidget, self).__init__(**kwargs)
+        print("INIT Width: " + str(self.width) + "Height: " + str(self.height))
+    # Widget needs to be visible before they can receive the focus. To initialize focus, you use the 'on_parent'
+    # event. on_open for a popup
+    def on_parent(self, widget, parent):
+
 class GalaxyApp(App):
+
     pass
 
 GalaxyApp().run()
